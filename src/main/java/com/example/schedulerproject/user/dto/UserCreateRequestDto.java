67b07b178565cc86425d0ece56dfc0a9c96c893c
dain <0901ekdls@gmail.com>
2025-05-25 19:25:51 +0900

    feat: implement user creation logic for Lv2

--- src/main/java/com/example/schedulerproject/user/dto/UserCreateRequestDto.java
+++ src/main/java/com/example/schedulerproject/user/dto/UserCreateRequestDto.java
@@ -1,8 +1,29 @@
 package com.example.schedulerproject.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /*
     유저 생성 요청용 DTO
     - 클라이언트가 유저 생성 시 보내는 데이터 구조
 */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserCreateRequestDto {
+
+    @NotBlank(message = "유저명은 필수 입력값입니다.")
+    @Size(max = 20, message = "유저명은 최대 20자까지 가능합니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "유효한 이메일 형식이어야 합니다.")
+    @Size(max = 50, message = "이메일은 최대 50자까지 가능합니다.")
+    private String email;
 }
--- src/main/java/com/example/schedulerproject/user/entity/User.java
+++ src/main/java/com/example/schedulerproject/user/entity/User.java
@@ -1,9 +1,43 @@
 package com.example.schedulerproject.user.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
 /*
     DB의 users 테이블과 매핑되는 클래스
     - 유저명, 이메일, 생성일, 수정일 속성
     - JPA Auditing을 통해 생성일과 수정일 자동 기록
 */
+@Entity // 이클래스가 JPA 엔티티임을 나타냄
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor // 기본 생성자 생성
+@AllArgsConstructor // 모든 필드를 포함한 생성자 생성
+@Builder // 객체 생성 시 빌더 패턴 사용 가능
+@EntityListeners(AuditingEntityListener.class) // 생성, 수정 시간 자동 반영을 위한 리스너 등록
 public class User {
+
+    @Id // 기본 키(PK) 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 전략
+    private Long id; // 유저 고유 ID
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 50, unique = true) // 중복 불가
+    private String email; // 이메일
+
+    @CreatedDate // 엔티티 생성 시 자동 저장
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt; // 생성일
+
+    @LastModifiedDate // 엔티티 수정시 자동 갱신
+    @Column(nullable = false)
+    private LocalDateTime updatedAt; // 수정일
 }
